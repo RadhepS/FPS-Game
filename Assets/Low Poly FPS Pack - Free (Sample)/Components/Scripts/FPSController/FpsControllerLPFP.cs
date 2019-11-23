6943e07eb1a0b9e1169ffe2578d928edb161df7e
Radhep <Radhep.Saba@gmail.com>
2019-11-23 13:25:49 -0500

    changes

--- Assets/Low Poly FPS Pack - Free (Sample)/Components/Scripts/FPSController/FpsControllerLPFP.cs
+++ Assets/Low Poly FPS Pack - Free (Sample)/Components/Scripts/FPSController/FpsControllerLPFP.cs
@@ -327,7 +327,7 @@ namespace FPSControllerLPFP
                     }
                 }
                 counter++;
-                Debug.Log(counter);
+                Debug.Log(CountOfPlayers);
 
             }
 
